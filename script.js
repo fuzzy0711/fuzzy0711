ac8c81a7cbe4fec6359d25f5e00334bcc514b4bb
[Mohammed Fuzail Malik] <[fuzailmalik2000@gmail.com]>
2023-03-02 20:56:02 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+    function alertpop(){
+        // alert('BUTTON CLICKED');
+        var ele=document.getElementById('heading_1');
+        ele.innerHTML='Button has been Clicked';
+        
+    }
